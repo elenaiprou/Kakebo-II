e55398f6eda2b8e1de33785fb908f1e736750642
elenaiprou <elenaiprou@gmail.com>
2021-06-10 23:13:44 +0200

    first commit

---
+++ kakebo/views.py
@@ -0,0 +1,22 @@
+import sqlite3
+from flask import jsonify, render_template
+from kakebo import app
+from kakebo.dataaccess import DBmanager
+
+dbManager = DBmanager(app.config.get('DATABASE'))
+
+@app.route('/')
+def listaMovimientos():
+    return render_template('spa.html')
+
+@app.route('/api/v1/movimientos')
+def movimientosAPI():
+    query = "SELECT * FROM movimientos ORDER BY fecha;"
+
+    try:
+        lista = dbManager.consultaMuchasSQL(query)
+        return jsonify({'status': 'success', 'movimientos': lista})
+    except sqlite3.Error as e:
+        return jsonify({'status': 'fail', 'mensaje': str(e)})
+
+@app.route()
---
+++ run.py
@@ -0,0 +1,2 @@
+from kakebo import app
+
