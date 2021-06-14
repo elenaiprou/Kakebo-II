d963d6c4e324d095343d618cc4e4259d107a0faa
elenaiprou <elenaiprou@gmail.com>
2021-06-14 23:48:26 +0200

    ultima clase

--- kakebo/static/js/spa.js
+++ kakebo/static/js/spa.js
@@ -7,28 +7,52 @@ const categoria = {
 
 let losMovimientos //variable global sin nada, para guardar los movimientos "pa' luego"
 
+xhr = new XMLHttpRequest()
+xhr.onload = muestraMovimientos
+
+function recibeRespuesta() {
+    if (this.readyState === 4 && (this.status ===200 || this.status === 201)) {
+        const respuesta = JSON.parse(this.responseText)
+
+        if (respuesta.status != "success"){
+            alert("Se ha producido un error en acceso a servidor " + respuesta.mensaje)
+            return
+        }
+
+        alert(respuesta.mensaje)
+
+        llamaApiMovimientos()
+    }
+}
+
 function detallaMovimiento(id) {
+
+    //movimiento = losMovimientos.filter(item => item.id == id )[0]
+
     let movimiento
     for (let i=0; i<losMovimientos.length; i++) {
         const item = losMovimientos[i]
-        if (item.id == id) {
+        if (item.id == id ) {
             movimiento = item
             break
         }
     }
 
+
     if (!movimiento) return //esto es si la variable movimiento no existe.
 
+    document.querySelector("#idMovimiento").value = id
     document.querySelector("#fecha").value = movimiento.fecha
     document.querySelector("#concepto").value = movimiento.concepto
     document.querySelector("#categoria").value = movimiento.categoria
-    document.querySelector("#cantidad").value = movimiento.cantidad.toFixed(2) //toFixe pone 2 decimales
+    document.querySelector("#cantidad").value = movimiento.cantidad.toFixed(2)
     if (movimiento.esGasto == 1) {
         document.querySelector("#gasto").checked = true
     } else {
         document.querySelector("#ingreso").checked = true
     }
 }
+
 
 function muestraMovimientos() {
     if (this.readyState === 4 && this.status === 200) {
@@ -40,6 +64,8 @@ function muestraMovimientos() {
         }
 
         losMovimientos = respuesta.movimientos
+        const tbody = document.querySelector(".tabla-movimientos tbody")
+        tbody.innerHTML = ""
 
         for (let i = 0; i < respuesta.movimientos.length; i++) {
             const movimiento = respuesta.movimientos[i]
@@ -56,21 +82,131 @@ function muestraMovimientos() {
                 <td>${movimiento.cantidad} €</td>
             `
             fila.innerHTML = dentro
-            const tbody = document.querySelector(".tabla-movimientos tbody")
+
             tbody.appendChild(fila)
         }
     }
 }
 
-xhr = new XMLHttpRequest()
-xhr.onload = muestraMovimientos
-
-function llamarApiMovimientos() {
+function llamaApiMovimientos() {
     xhr.open('GET', `http://localhost:5000/api/v1/movimientos`, true)
+    xhr.onload = muestraMovimientos
     xhr.send()
 }
 
-window.onload = function () {
-    llamarApiMovimientos()
+function capturaFormMovimiento() {
+    const movimiento = {}
+    movimiento.fecha = document.querySelector("#fecha").value
+    movimiento.concepto = document.querySelector("#concepto").value
+    movimiento.categoria = document.querySelector("#categoria").value
+    movimiento.cantidad = document.querySelector("#cantidad").value
+    if (document.querySelector("#gasto").checked) {
+        movimiento.esGasto = 1
+    } else {
+        movimiento.esGasto = 0
+    }
+    return movimiento
+}
+
+function validar(movimiento) {
+    if (!movimiento.fecha) {
+        alert("fecha obligatoria")
+        return false
+    }
+
+    if (movimiento.concepto === "") {
+        alert("Se ha de informar del concepto")
+        return false
+    }
+
+    if (document.querySelector("#gasto").checked && !document.querySelector("#ingreso").checked) {
+        alert("Elija tipo de movimiento")
+        return false
+    }
+
+    if (movimiento.esGasto && !movimient.categoria) {
+        alert("Debe seleccionar categoria del gasto")
+        return false
+    }
+
+    if (!movimiento.esGasto && movimient.categoria) {
+        alert("Un ingewao no puede tener categoria")
+        return false
+    }
+    
+    if (movimiento.cantidad <= 0) {
+        alert("Cantidad ha de ser positiva")
+        return false
+    }
+
+    return true
+}
+
+function llamaApiModificaMovimiento(ev) {
+    ev.preventDefault()
+    id = document.querySelector("#idMovimiento").value
+    if (!id) {
+        alert("Selecciona un movimiento antes!")
+        return
+    }
+
+    const movimiento = capturaFormMovimiento ()
+    if (!validar(movimiento)) {
+        return
+    }
+
+    xhr.open("PUT", `http://localhost:5000/api/v1/movimiento/${id}`, true)
+    xhr.onload = recibeRespuesta
+
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+
+    xhr.send(JSON.stringify(movimiento))
+}
+
+/*sepuede llamar funcion así:
+const llamaApiBorraMovimiento = (ev) => {
+}
+*/
+
+function llamaApiBorraMovimiento(ev) {
+    ev.preventDefault()
+    id = document.querySelector("#idMovimiento").value
+    if (!id) {
+        alert("Selecciona un movimiento antes!")
+        return
+    }
+
+    xhr.open("DELETE", `http://localhost:5000/api/v1/movimiento/${id}`, true)
+    xhr.onload = recibeRespuesta
+    xhr.send()
+}
+
+function llamaApiCreaMovimiento(ev) {
+    ev.preventDefault()
+    const movimiento = capturaFormMovimiento ()
+    if (!validar(movimiento)) {
+        return
+    }
+
+
+    xhr.open("POST", `http://localhost:5000/api/v1/movimiento`, true)
+    xhr.onload = recibeRespuesta
+
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+    
+    xhr.send(JSON.stringify(movimiento))
+}
+
+window.onload = function() {
+    llamaApiMovimientos()
+
+    document.querySelector("#modificar")
+        .addEventListener("click", llamaApiModificaMovimiento)
+    
+    document.querySelector("#borrar")
+        .addEventListener("click", llamaApiBorraMovimiento)
+    
+        document.querySelector("#crear")
+        .addEventListener("click", llamaApiCreaMovimiento)
 
 }
--- kakebo/views.py
+++ kakebo/views.py
@@ -1,7 +1,9 @@
-import sqlite3
-from flask import jsonify, render_template
 from kakebo import app
 from kakebo.dataaccess import DBmanager
+from flask import jsonify, render_template, request
+import sqlite3
+from http import HTTPStatus
+
 
 dbManager = DBmanager(app.config.get('DATABASE'))
 
@@ -19,4 +21,47 @@ def movimientosAPI():
     except sqlite3.Error as e:
         return jsonify({'status': 'fail', 'mensaje': str(e)})
 
-@app.route()
+@app.route('/api/v1/movimiento/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+@app.route('/api/v1/movimiento', methods=['POST'])
+def detalleMovimiento(id=None):
+
+    try:
+        if request.method in ('GET', 'PUT', 'DELETE'):
+            movimiento = dbManager.consultaUnaSQL("SELECT * FROM movimientos WHERE id = ?", [id])
+        
+        if request.method == 'GET':
+            if movimiento:
+                return jsonify({
+                    "status": "success",
+                    "movimiento": movimiento
+                })
+            else:
+                return jsonify({"status": "fail", "mensaje": "movimiento no encontrado"}), HTTPStatus.NOT_FOUND
+
+        if request.method == 'PUT':
+            dbManager.modificaTablaSQL("""
+                UPDATE movimientos 
+                SET fecha=:fecha, concepto=:concepto, esGasto=:esGasto, categoria=:categoria, cantidad=:cantidad 
+                WHERE id = {}""".format(id), request.json)
+            
+            return jsonify({"status": "success", "mensaje": "registro modificado"})
+        
+        if request.method == 'DELETE':
+            dbManager.modificaTablaSQL("""
+                DELETE FROM movimientos
+                WHERE id = ?""", [id])
+
+            return jsonify({"status": "success", "mensaje": "registro borrado"})
+
+        if request.method == 'POST':
+            dbManager.modificaTablaSQL("""
+                INSERT INTO movimientos
+                    (fecha, concepto, esGasto, categoria, cantidad)
+                VALUES (:fecha, :concepto, :esGasto, :categoria, :cantidad)
+                """, request.json)
+
+            return jsonify({"status": "success", "mensaje": "registro creado"}), HTTPStatus.CREATED
+
+    except sqlite3.Error as e:
+        print("BBDD error:", e)
+        return jsonify({"status": "fail", "mensaje": "Error en base de datos: {}".format(e)}), HTTPStatus.BAD_REQUEST
